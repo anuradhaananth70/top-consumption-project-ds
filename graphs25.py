6a66c5408843497ddbbf75496cae70e282a803bb
anuradhaananth70 <161723379+anuradhaananth70@users.noreply.github.com>
2024-03-06 15:34:31 +0530

    Update graphs25.py

--- graphs25.py
+++ graphs25.py
@@ -230,6 +230,8 @@ colors = ['#8FB5A5', '#FFD5C2', '#A1C7C3', '#D7C4BB']  # Subdued colors
 fig1, ax1 = plt.subplots(figsize=(1, 1))  # Adjust the size as needed
 ax1.pie(sizes, colors=colors, labels=labels, autopct='%1.1f%%', startangle=90, textprops={'fontsize': 2})
 ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+st.pyplot(fig1, use_container_width=True)  # Use use_container_width parameter
+
 # Set the font size of the legend
 ax1.legend(labels, loc="center right", fontsize=2)
 # Display the pie chart on the left side of the Streamlit app
