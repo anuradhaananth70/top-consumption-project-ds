a1144343c405acf8130d2f1e17f2a58ea1cdebd3
anuradhaananth70 <161723379+anuradhaananth70@users.noreply.github.com>
2024-03-06 15:31:44 +0530

    Update graphs25.py

--- graphs25.py
+++ graphs25.py
@@ -227,8 +227,8 @@ sizes = [14, 34, 42, 10]
 colors = ['#8FB5A5', '#FFD5C2', '#A1C7C3', '#D7C4BB']  # Subdued colors
 
 # Create a pie chart with a smaller size
-fig1, ax1 = plt.subplots(figsize=(3, 3))  # Adjust the size as needed
-ax1.pie(sizes, colors=colors, labels=labels, autopct='%1.1f%%', startangle=90, textprops={'fontsize': 8})
+fig1, ax1 = plt.subplots(figsize=(2, 2))  # Adjust the size as needed
+ax1.pie(sizes, colors=colors, labels=labels, autopct='%1.1f%%', startangle=90, textprops={'fontsize': 4})
 ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 # Set the font size of the legend
 ax1.legend(labels, loc="center right", fontsize=8)
